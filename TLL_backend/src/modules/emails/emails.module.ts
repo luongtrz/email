b7bc4e9d07991a231c1fb15635baedf61e920ca3
Huỳnh Tấn Lộc <locht1@runsystem.net>
2025-12-17 10:42:51 +0700

    feat: Implement email management API including pseudo-fuzzy search, send, reply, forward, modify, and attachment download functionalities.

--- TLL_backend/src/modules/emails/emails.module.ts
+++ TLL_backend/src/modules/emails/emails.module.ts
@@ -3,6 +3,7 @@ import { EmailsController } from './emails.controller';
 import { EmailsService } from './emails.service';
 import { GmailApiService } from './services/gmail-api.service';
 import { GmailParserService } from './services/gmail-parser.service';
+import { GmailSearchService } from './services/gmail-search.service';
 import { GmailAuthGuard } from './guards/gmail-auth.guard';
 import { AuthModule } from '../auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -11,7 +12,19 @@ import { User } from '@/database/entities/user.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([User]), AuthModule],
   controllers: [EmailsController],
-  providers: [EmailsService, GmailApiService, GmailParserService, GmailAuthGuard],
-  exports: [EmailsService, GmailApiService, GmailParserService, GmailAuthGuard],
+  providers: [
+    EmailsService,
+    GmailApiService,
+    GmailParserService,
+    GmailSearchService,
+    GmailAuthGuard,
+  ],
+  exports: [
+    EmailsService,
+    GmailApiService,
+    GmailParserService,
+    GmailSearchService,
+    GmailAuthGuard,
+  ],
 })
 export class EmailsModule {}
